45b2c873ae83d65b6a29fb4ff1045635d7912305
Stefan Young <syoung2019@gatech.edu>
2017-04-22 19:39:38 -0400

    fixed key error stemming from re-running procedure on test and train

--- extract_labels.py
+++ extract_labels.py
@@ -3,36 +3,38 @@ import numpy as np
 
 pd.set_option('display.max_columns', None)
 
-category_values = {}
-category_numbers= {}
-category_type   = {}
-names_in_order  = []
-lines = open('data/data_values.txt', 'r').readlines()
-for line in lines:
-    name   = line.split(':')[0]
-    if name == '| instance weight':
-        continue
-    classes = [s.strip() for s in line.split(':')[1].split(',')]
-    # values
-    classes[-1] = classes[-1][:-1]
-    category_values[name] = classes
-    # num
-    category_numbers[name] = len(classes)
-    # category_type
-    category_type[name] = 'continuous' if classes[0] == 'continuous' else 'nominal'
-    # names in order
-    names_in_order.append(name)
-
-class_mapping = {}
-for name in names_in_order:
-    if category_type == 'continuous':
-        class_mapping[name] = 0
-    else:
-        class_mapping[name] = {}
-        for i in range(category_numbers[name]):
-            class_mapping[name][category_values[name][i]] = i
 
 def extract_data(filename):
+    category_values = {}
+    category_numbers= {}
+    category_type   = {}
+    names_in_order  = []
+    lines = open('data/data_values.txt', 'r').readlines()
+    for line in lines:
+        name   = line.split(':')[0]
+        if name == '| instance weight':
+            continue
+        classes = [s.strip() for s in line.split(':')[1].split(',')]
+        # values
+        classes[-1] = classes[-1][:-1]
+        category_values[name] = classes
+        # num
+        category_numbers[name] = len(classes)
+        # category_type
+        category_type[name] = 'continuous' if classes[0] == 'continuous' else 'nominal'
+        # names in order
+        names_in_order.append(name)
+
+    class_mapping = {}
+    for name in names_in_order:
+        if category_type == 'continuous':
+            class_mapping[name] = 0
+        else:
+            class_mapping[name] = {}
+            for i in range(category_numbers[name]):
+                class_mapping[name][category_values[name][i]] = i
+
+
     # 24 is instance weights, we want to ignore it
     cols = [i for i in range(24)] + [i for i in range(25,len(names_in_order) + 1)]
     print('Starting Reading ' + filename)
@@ -41,7 +43,7 @@ def extract_data(filename):
 
     # cleanse last column
     print('Cleansing income column')
-    remove_period = lambda x: x.replace('.','')
+    remove_period = lambda x: str(x).replace('.','')
     df['income'] = df['income'].apply(remove_period)
     # get total number of hot bits
     column_vals = list(df.columns.values)
@@ -88,17 +90,17 @@ def extract_data(filename):
                 index += 1
     return X, labels
 
+# TEST
 train_filename = 'data/census-income.data'
 X_train, y_train = extract_data(train_filename)
 print('Saving Training Data')
 np.save('data/training_data.npy', X_train)
 np.save('data/training_labels.npy', y_train)
 
-
+# TRAIN
 test_filename = 'data/census-income.test'
 X_test, y_test = extract_data(test_filename)
-# SAVE DATA
-print('Saving Training Data')
-np.save('data/training_data.npy', X_test)
-np.save('data/training_labels.npy', y_test)
+print('Saving Test Data')
+np.save('data/testing_data.npy', X_test)
+np.save('data/testing_labels.npy', y_test)
 
